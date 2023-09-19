bafac43f43499e2cae4d79fcc7ba7056cf4ee3fa
JoaquinDicio <97567954+JoaquinDicio@users.noreply.github.com>
2023-09-19 16:58:01 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 function main() {
   setHeader();
+  setSlider()
 }
 
+//header and menu
 function setHeader() {
   setHeaderScrollEffect();
   setHeaderResponsive();
@@ -26,4 +28,30 @@ function setHeaderResponsive() {
 function toggleShowMenu() {
   const nav = document.querySelector(".nav-responsive");
   nav.classList.toggle("d-block");
+}
+
+//slider
+let sliderIdx = 0
+
+function setSlider() {
+  const next = document.querySelector('#next')
+  const back = document.querySelector('#back')
+  next.addEventListener('click', sliderNext)
+  back.addEventListener('click', sliderPrev)
+}
+
+function sliderNext() {
+  const cantImgs = document.querySelectorAll('.reparto-card').length
+  if (sliderIdx !== (cantImgs-1)) sliderIdx++
+  moveSlider()
+}
+
+function moveSlider(){
+  const slider = document.querySelector('#slider')
+  slider.style.transform = `translateX(-${sliderIdx * 340}px)`;
+}
+
+function sliderPrev() {
+  if (sliderIdx !==0) sliderIdx--
+  moveSlider()
 }
